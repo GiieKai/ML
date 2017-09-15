26ffce51ba69a79c62579cdec2602d72062199ce
LeoHung <weikai0814@gmail.com>
2017-09-15 19:59:37 +0800

    first commit

---
+++ hw0/Q1.py
@@ -0,0 +1,23 @@
+result=[]
+with open('words.txt','r') as f:
+    for line in f:
+        result = line.split()
+s = []
+for i in result:
+    if i not in s:
+        s.append(i)
+
+counttt = []
+for i in s:
+    counttt.append(result.count(i))
+
+txtlist = []
+for ii in range(300):
+    txtlist.append(s[ii] +' '+ str(ii) +' '+ str(counttt[ii]))
+
+thefile = open('Q1.txt', 'w')
+for item in txtlist:
+    thefile.write(item)
+    if txtlist.index(item) <299:
+        thefile.write('\n')
+thefile.close()
---
+++ hw0/Q2.py
@@ -0,0 +1,13 @@
+from PIL import Image
+im = Image.open( "westbrook.jpg" )
+pix = im.load()
+r, g, b = im.split()
+rpix = r.load()
+gpix = g.load()
+bpix = b.load()
+#im2 = Image.new(im.mode, im.size)
+for i in range(im.size[0]):    # for every col:
+    for j in range(im.size[1]):    # For every row
+        pix[i,j] = (int(rpix[i,j]/2), int(gpix[i,j]/2), int(bpix[i,j]/2)) # set the colour accordingly
+        im.putpixel((i,j),pix[i,j])
+im.save('Q2.png')
